790a00dfa5fd493ef6dfc1305a196c141b95a7c1
yosogo <80201392+yosogo@users.noreply.github.com>
2022-04-02 15:31:24 +0300

    Delete abdubot.js

--- abdubot.js
+++
@@ -1,9 +0,0 @@
-var TelegramBot = require('node-telegram-bot-api');
-
-var token = '1745323024:AAF8NJQfOlMOPL02YHbvXALL5BozJK6WOMw';
-var bot = new TelegramBot(token, {polling: true});
-bot.on("polling_error", (err) => console.log(err));
-bot.on("message",function(msg){
- bot.sendMessage(msg.chat.id,rep, {text});
-
-});
