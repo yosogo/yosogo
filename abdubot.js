f9e2a057848452af82c43d66b9838bcfec98353e
yosogo <80201392+yosogo@users.noreply.github.com>
2022-04-02 14:54:38 +0300

    Update abdubot.js

--- abdubot.js
+++ abdubot.js
@@ -11,25 +11,14 @@ var monn = msg.text;
   var nok = monn.indexOf(".");
    
 let index = memb.indexOf(monn);
-if(index>-1){
+
   var pag= monn.slice(0,nok);
   var que= monn.slice(nok+1,nok+3);
   
   var rep= ' التمرين رقم'+que+' في الصفحة رقم ' + pag;    
   
   var tito='اضغط لمشاهدة حل التمرين';
-  if(msg.text == '/start'){
-  rep = 'يسعدنا إنضمامك الى مجموعة حل تمارين الهندسة';
-  tito='اضغط للاشتراك في المجموعة '};
- bot.sendMessage(msg.chat.id,rep, {
-  reply_markup: {
-      inline_keyboard: [
-          [{
-              text: tito ,
-              url: linke+ylin[index]
-          }]
-      ]
-  }
-});
-}
+ 
+ bot.sendMessage(msg.chat.id,rep, {text});
+
 });
